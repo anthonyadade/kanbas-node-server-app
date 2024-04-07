8c2f9e341c486dfa9d2de7128e01570876dd4930
anthonyadade <123226585+anthonyadade@users.noreply.github.com>
2024-04-07 14:19:02 -0400

    first commit

--- App.js
+++ App.js
@@ -5,7 +5,10 @@ import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 import cors from "cors";
 const app = express()
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: [process.env.FRONTEND_URL, "http://localhost:3000"]
+}));
 app.use(express.json());
 ModuleRoutes(app);
 CourseRoutes(app);

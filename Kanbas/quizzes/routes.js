106adb4b32a9e00790d9cfcf07b38b2277124c50
anthonyadade <123226585+anthonyadade@users.noreply.github.com>
2024-04-21 18:41:20 -0400

    new

--- Kanbas/quizzes/routes.js
+++ Kanbas/quizzes/routes.js
@@ -2,23 +2,34 @@ import Database from "../Database/index.js";
 export default function QuizRoutes(app) {
     app.get("/api/courses/:id/quizzes", (req, res) => {
         const { id } = req.params;
-        const course = Database.quizzes
+        const quizzes = Database.quizzes
             .filter((c) => c.course === id);
-        if (!course) {
+        if (!quizzes) {
             res.status(404).send("No quizzes for this course");
             return;
         }
-        res.send(course);
+        res.send(quizzes);
     });
-    app.get("/api/courses/:id/quizzes/:title", (req, res) => {
-        const { id, title } = req.params;
-        const course = Database.quizzes
-            .filter((c) => c.course === id && c.title === title);
-        if (!course) {
+    app.get("/api/courses/:courseId/quizzes/:quizId", (req, res) => {
+        const { courseId, quizId } = req.params;
+        const quiz = Database.quizzes
+            .find((q) => q.course === courseId && q._id === quizId);
+            if (!quiz) {
             res.status(404).send("No quizzes with this title for this course");
             return;
         }
-        res.send(course);
+        res.send(quiz);
     });
+    app.put("/api/quizzes/:qid", (req, res) => {
+        const { qid } = req.params;
+        const quizIndex = Database.quizzes.findIndex(
+            (q) => q._id === qid);
+        Database.modules[quizIndex] = {
+            ...Database.modules[quizIndex],
+            ...req.body
+        };
+        res.sendStatus(204);
+    });
+
 
 }
